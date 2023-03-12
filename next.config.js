a8872541b3d97fe7e2f2d0679f9d9e8ccebca9a3
Aksel Glyholt <akselglyholt1@gmail.com>
2023-03-12 16:27:32 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  plugins: [
+    require("tailwindcss-textshadow"),
+    require("tailwind-scrollbar-hide"),
+    require('tailwind-scrollbar'),
+  ],
+};
